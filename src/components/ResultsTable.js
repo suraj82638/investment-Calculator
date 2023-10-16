562df5ba241f4a65a956e04b49f14d00c744a81a
Suraj <suraj82638@gmail.com>
2023-10-16 21:53:58 +0530

    final

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import classes from './ResultsTable.module.css';
 
-const ResultsTable = () => {
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const ResultsTable = (props) => {
   return (
-    <div>
-          <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
+    <table className={classes.result}>
+    <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((yearData) => (
+          <tr key={yearData.year}>
+            <td>{yearData.year}</td>
+            <td>{formatter.format(yearData.savingsEndOfYear)}</td>
+            <td>{formatter.format(yearData.yearlyInterest)}</td>
+            <td>
+              {formatter.format(
+                yearData.savingsEndOfYear -
+                  props.initialInvestment -
+                  yearData.yearlyContribution * yearData.year
+              )}
+            </td>
+            <td>
+              {formatter.format(
+                props.initialInvestment +
+                  yearData.yearlyContribution * yearData.year
+              )}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  )
-}
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-export default ResultsTable
+export default ResultsTable;
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,45 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const UserInput = () => {
-  const initialState = {
-    'current-savings': 10000,
-    'yearly-contribution': 5000,
-    'expected-return': 40,
-    'duration': 7
-  }
+const initialState = {
+  "current-savings": 10000,
+  "yearly-contribution": 5000,
+  "expected-return": 40,
+  duration: 7,
+};
 
-  const [userInputValue, setUserInputValue] = useState(initialState)
+const UserInput = (props) => {
+  const [userInput, setUserInput] = useState(initialState);
 
-  const submitHandler = (e) =>{
+  const submitHandler = (e) => {
     e.preventDefault();
-    console.log("submit")
-  }
+    props.onCalculate(userInput);
+  };
 
-const resetHandler = () => {
-  setUserInputValue(initialState)
-}
+  const resetHandler = () => {
+    setUserInput(initialState);
+  };
 
-const inputChangeHandler = (input, value) =>{
-  setUserInputValue((prevState)=> {
-   return {
-    ...prevState, [input]: value
-   }
-  } )
-
-}
-
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [input]: value,
+      };
+    });
+  };
 
   return (
     <div>
-        <form onSubmit={submitHandler} className="form">
+      <form onSubmit={submitHandler} className="form">
         <div className="input-group">
           <p>
             <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" onChange={(e)=>{inputChangeHandler('current-savings', e.target.value)}} value={userInputValue['current-savings']} id="current-savings" />
+            <input
+              type="number"
+              onChange={(e) => {
+                inputChangeHandler("current-savings", e.target.value);
+              }}
+              value={userInput["current-savings"]}
+              id="current-savings"
+            />
           </p>
           <p>
             <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number"  onChange={(e)=>{inputChangeHandler('yearly-contribution', e.target.value)}} value={userInputValue['yearly-contribution']} id="yearly-contribution" />
+            <input
+              type="number"
+              onChange={(e) => {
+                inputChangeHandler("yearly-contribution", e.target.value);
+              }}
+              value={userInput["yearly-contribution"]}
+              id="yearly-contribution"
+            />
           </p>
         </div>
         <div className="input-group">
@@ -47,15 +60,29 @@ const inputChangeHandler = (input, value) =>{
             <label htmlFor="expected-return">
               Expected Interest (%, per year)
             </label>
-            <input type="number"  onChange={(e)=>{inputChangeHandler('expected-return', e.target.value)}} value={userInputValue['expected-return']} id="expected-return" />
+            <input
+              type="number"
+              onChange={(e) => {
+                inputChangeHandler("expected-return", e.target.value);
+              }}
+              value={userInput["expected-return"]}
+              id="expected-return"
+            />
           </p>
           <p>
             <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number"  onChange={(e)=>{inputChangeHandler('duration', e.target.value)}} value={userInputValue['duration']} id="duration" />
+            <input
+              type="number"
+              onChange={(e) => {
+                inputChangeHandler("duration", e.target.value);
+              }}
+              value={userInput["duration"]}
+              id="duration"
+            />
           </p>
         </div>
         <p className="actions">
-          <button onClick = {resetHandler} type="reset" className="buttonAlt">
+          <button onClick={resetHandler} type="reset" className="buttonAlt">
             Reset
           </button>
           <button type="submit" className="button">
@@ -64,7 +91,7 @@ const inputChangeHandler = (input, value) =>{
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default UserInput
+export default UserInput;
